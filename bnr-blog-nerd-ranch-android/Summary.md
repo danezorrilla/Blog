f2314256a255819e02883abfbfafd24e2ef6dd88
Daniel Zorrilla <danezorrilla@gmail.com>
2023-07-26 17:30:26 -0400

    update summary

--- bnr-blog-nerd-ranch-android/Summary.md
+++ bnr-blog-nerd-ranch-android/Summary.md
@@ -1,13 +1,16 @@
-#Summary
+# Summary
 
-This application took a lot longer to complete than anticipated, At first there wasn't much
-difficulty, I transform the app architecture into MVVM. I create a repository to create our
-Retrofit instance and call the HTTP method. The ViewModel handled the HTTP request and sends the
-results to the View,  removing the networking duplications.
-I updated the UI so that for the PostMetaData the view will be in a CardView. Test files has been
-included, testing the network call. For loading the large data set things got complicated.
-I tried implemented coroutines but it didn't really improve the UX that much, I tired implementing
-Paging3 library with the same result, except the recyclerview keeps repopulating when I get to the
-end. If I had more time, I would work on fixing the bug as well as include Dagger Hilt for
-Dependency Injection, as well as include RoomDB to store the data into a local database, so we
-get the data from the database as oppose to the server
+- Transformed the app architecture into MVVM
+- Created a repository to create our Retrofit instance and call the HTTP method
+- Implemented ViewModel to handle the HTTP request and sends the results to the View,
+  removing the networking duplications.
+- Updated the UI so that for the PostMetaData, the view is in a CardView
+- Implemented Unit Testing and created custom test cases for network call
+- Implemented coroutines for asynchronous call
+- Implemented Paging3 library that helps to load and display small data at a time
+
+# If I had more time I would have implemented the following tasks
+
+
+- Dagger Hilt for Dependency Injection
+- RoomDB to store the data into a local database so that app will run in offline mode
--- bnr-blog-nerd-ranch-android/app/src/main/java/com/bignerdranch/android/blognerdranch/paging/PagingAdapter.kt
+++ bnr-blog-nerd-ranch-android/app/src/main/java/com/bignerdranch/android/blognerdranch/paging/PagingAdapter.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.blognerdranch.paging
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -7,25 +8,33 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.blognerdranch.R
 import com.bignerdranch.android.blognerdranch.model.Post
 
-//class PagingAdapter(val post: Post) : RecyclerView.Adapter<PagingAdapter.MainViewHolder>()  {
-//
-//    private lateinit var postList: ArrayList<Post>
-//
-//    class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-//        var title: TextView = itemView.findViewById(R.id.main_title_textview)
-//        var author: TextView = itemView.findViewById(R.id.main_author_textview)
-//        var body: TextView = itemView.findViewById(R.id.main_body_textview)
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun getItemCount(): Int {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-//        TODO("Not yet implemented")
-//    }
-//}
+class PagingAdapter(val post: Post) : RecyclerView.Adapter<PagingAdapter.MainViewHolder>()  {
+
+    private lateinit var postList: List<Post>
+
+    init {
+        postList = listOf<Post>(post)
+    }
+
+    class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var title: TextView = itemView.findViewById(R.id.main_title_textview)
+        var author: TextView = itemView.findViewById(R.id.main_author_textview)
+        var body: TextView = itemView.findViewById(R.id.main_body_textview)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.main_blog, parent, false)
+        return MainViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.title.text = postList[position].metadata?.title
+        holder.title.text = postList[position].metadata?.title
+        holder.title.text = postList[position].metadata?.title
+    }
+}
